d7f7d64d992bee2747f88a1c11320427df85634f
AndyLiu <dymy0627@hotmail.com>
2017-01-07 19:08:11 +0800

    add stock page.

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -7,54 +8,56 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class MainController 
-{
-	@FXML private Button order;
-	@FXML private Button purchase;
-	@FXML private Button daily;
-	@FXML private Button monthly;
-	
-	Stage stage;
-	Scene scene;
-	
-	//def fxml loader
-	Parent orderstage;
-	//Parent mainstage;
-	
-	@FXML protected void OrderButtonAction(ActionEvent event) 
-	{
-		try 
-		{
-			orderstage = FXMLLoader.load(getClass().getResource("/application/OrderStage.fxml"));
-		} 
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		//ref fxml to stage 
+public class MainController {
+	@FXML
+	private Button order;
+	@FXML
+	private Button purchase;
+	@FXML
+	private Button daily;
+	@FXML
+	private Button monthly;
+
+	private Stage stage;
+	private Scene scene;
+
+	@FXML
+	protected void OrderButtonAction(ActionEvent event) throws IOException {
+		// def fxml loader
+		Parent orderstage = FXMLLoader.load(getClass().getResource("/application/OrderStage.fxml"));
+
+		// ref fxml to stage
+		scene = new Scene(orderstage, 1024, 720);
 		stage = MainScene.stage_tmp;
-		scene = new Scene(orderstage,1024,720);
 		
-		//change scene to main scene
-	    stage.setScene(scene);
-	    stage.show();
-    }
-	
-	@FXML protected void PurchaseButtonAction(ActionEvent event) 
-	{
-        purchase.setText("Sign in button pressed2");
-    }
-	
-	@FXML protected void DailyButtonAction(ActionEvent event) 
-	{
-        daily.setText("Sign in button pressed3");
-    }
-	
-	@FXML protected void MonthlyButtonAction(ActionEvent event) 
-	{
-        monthly.setText("Sign in button pressed4");
-    }
+		// change scene to main scene
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@FXML
+	protected void PurchaseButtonAction(ActionEvent event) throws IOException {
+		// def fxml loader
+		Parent stockStage = FXMLLoader.load(getClass().getResource("/application/StockStage.fxml"));
+
+		// ref fxml to stage
+		scene = new Scene(stockStage, 1024, 720);
+		stage = MainScene.stage_tmp;
+		// change scene to main scene
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@FXML
+	protected void DailyButtonAction(ActionEvent event) {
+		daily.setText("Sign in button pressed3");
+	}
+
+	@FXML
+	protected void MonthlyButtonAction(ActionEvent event) {
+		monthly.setText("Sign in button pressed4");
+	}
 }
