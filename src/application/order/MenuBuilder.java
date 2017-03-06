e02b1ff3ac052366d89201094895eb3fabb44e65
AndyLiu <dymy0627@hotmail.com>
2017-03-06 21:37:01 +0800

    add menu to DB.

--- src/application/order/MenuBuilder.java
+++ src/application/order/MenuBuilder.java
@@ -10,11 +10,15 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import db.MySqlConnection;
+
 public class MenuBuilder {
 
 	private static Map<String, MealBean> mealMap = new HashMap<>();
 
 	public static void load() {
+//		MySqlConnection mySqlConnection = new MySqlConnection();
+//		mySqlConnection.connectSql();
 		System.out.println("--- meun loading start ---");
 		JSONParser parser = new JSONParser();
 		try {
@@ -28,13 +32,24 @@ public class MenuBuilder {
 				meal.setName((String) jsonObj.get("name"));
 				meal.setPrice(((Long) jsonObj.get("price")).intValue());
 				meal.setMeatClass((String) jsonObj.get("class"));
+				// insert to DB.
+//				insertMenuToDB(mySqlConnection, meal);
 				mealMap.put(id, meal);
-				System.out.println(id + ", " + meal.getSet() + "-" + meal.getName() + ", " + meal.getPrice()+ ", " + meal.getMeatClass());
+				System.out.println(id + ", " + meal.getSet() + "-" + meal.getName() + ", " + meal.getPrice() + ", "
+						+ meal.getMeatClass());
 			}
+//			mySqlConnection.disconnectSql();
+			
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}
 		System.out.println("--- meun loading end ---");
+	}
+
+	private static void insertMenuToDB(MySqlConnection mySqlConnection, MealBean meal) {
+		mySqlConnection.executeSql("insert into hamimelon.Menu(id, name, price, dish_set, meat_class)values('"
+				+ meal.getId() + "','" + meal.getName() + "','" + meal.getPrice() + "','" + meal.getSet() + "','"
+				+ meal.getMeatClass() + "')");
 	}
 
 	public static MealBean getMealById(String id) {
